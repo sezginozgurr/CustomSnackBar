20ac23ca7e569273a91d7b26aae947c9a55e312b
sezginozgurr <sezginzgurr@gmail.com>
2021-04-30 23:27:25 +0300

    OnTouch Control completed

--- app/src/main/java/com/example/customseekbar/CustomSnackBar.kt
+++ app/src/main/java/com/example/customseekbar/CustomSnackBar.kt
@@ -6,8 +6,10 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import java.lang.Math.abs
 
 class CustomSnackBar @JvmOverloads constructor(
     context: Context,
@@ -28,6 +30,9 @@ class CustomSnackBar @JvmOverloads constructor(
     private val thumbCircleRadius =
         context.resources.getDimensionPixelSize(R.dimen.thumb_circle_radius)
     private val thumbCircleRectangle = RectF()
+
+    private val min = -50
+    private val max = 50
 
     init {
         circlePaint.apply {
@@ -64,19 +69,30 @@ class CustomSnackBar @JvmOverloads constructor(
         //Todo elimizi koyduğumuz da ki hareketler geliyor
         when (event.action) {
             MotionEvent.ACTION_DOWN -> { //Todo Kullanıcı ilk parmağını dokunduğu noktalar
-
+                calculateProgress(event)
                 //Todo dokunduğun noktaya taşıyacağım. Sadece sol ve sağını değiştireceğiz.
                 thumbCircleRectangle.left = event.x - thumbCircleRadius
                 thumbCircleRectangle.right = event.x + thumbCircleRadius
                 invalidate() // Şuanki halini invalidate yap hareketi yakalarsın ondraw cağrılmadığı için anlam ifade etmez kesinlikle invalidate yapman lazım.
             }
             MotionEvent.ACTION_MOVE -> {
+                calculateProgress(event)
                 thumbCircleRectangle.left = event.x - thumbCircleRadius
                 thumbCircleRectangle.right = event.x + thumbCircleRadius
                 invalidate() //Todo tıklama ile değil kaydırma ile yapmak için
             }
         }
         return true
+    }
+
+    private fun calculateProgress(event: MotionEvent) {
+        val multiplier = if (event.x < viewRect.centerX()) min else max
+
+        val difference = abs(event.x - viewRect.centerX())
+
+        val progress = difference / viewRect.centerX() * multiplier
+
+        Log.d("Tag", progress.toString())
     }
 
     override fun onDraw(canvas: Canvas) { //Todo resim kagıdının tamamı canvas.. Bunun üzerine pozisyon yerlerini belirteerek birşey çizeceğiz
